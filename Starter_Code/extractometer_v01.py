c44daff82057d96424a9c2ed251b708ff6eac54c
CSM <34834285+rendorHaevyn@users.noreply.github.com>
2018-07-30 12:15:26 +1000

    Extractometer now splits files by Mode and On/Off
    > Mode split (1=bus,2=train,3=tram,other=other)
    > State split (on=touchon,off=touchoff)
    > Not implemented: split by year

--- Starter_Code/extractometer_v01.py
+++ Starter_Code/extractometer_v01.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[27]:
+# In[imports]:
 
 from __future__ import print_function
 import os
@@ -9,77 +9,79 @@ import gzip
 import threading
 import sys
 
+# In[constants]:
 
-# In[29]:
-
-WKDIR = 'C:\\Users\\Admin\\Documents\\MelbDatathon2018'
+WKDIR = "H://Downloads//adjustedr2"
+#WKDIR = 'C:\\Users\\Admin\\Documents\\MelbDatathon2018'
 os.chdir(WKDIR)
 
-fo_s0on = 'samp0_on'
-fo_s0off = 'samp0_off'
-
-fo_s1on = 'samp1_on'
-fo_s1off = 'samp1_off'
-
-scan_cnt = {'samp0_on':0, 'samp0_off':0, 'samp1_on':0, 'samp1_off':0}
-scan_headers = 'Mode|BusinessDate|DateTime|CardID|CardType|ParentRoute|VehicleID|RouteID|StopID\r\n'
+files_out = {
+        '1On':'On_Bus'
+        ,'1Off':'Off_Bus'
+        ,'2On':'On_Train'       
+        ,'2Off':'Off_Train'
+        ,'3On':'On_Tram'
+        ,'3Off':'Off_Tram'
+        ,'OthOn':'On_Other'
+        ,'OthOff':'Off_Other'
+        }
+scan_cnt = {
+        '1On':0
+        ,'1Off':0
+        ,'2On':0
+        ,'2Off':0
+        ,'3On':0
+        ,'3Off':0
+        ,'OthOn':0
+        ,'OthOff':0
+        }
+scan_headers = 'Mode|BusinessDate|DateTime|CardID|CardType|ParentRoute|VehicleID|RouteID|StopID' + os.linesep
 
 threads = []
 
+# In[file setup]:
+# Make the placeholder files
+for v in files_out.values():
+    with open(os.path.join(WKDIR,v), 'wb') as f_out:
+        f_out.write(scan_headers.encode())
 
-# In[40]:
-
-def scan_create(root,f,f_scan):
+# In[file scan def]:
+def scan_create(root,f,scan_tp):
     global scan_cnt
     print(f)
-    if not scan_cnt[f_scan]: # create file on first of type
-        with gzip.open(os.path.join(root,f), 'rb') as f_in, open(os.path.join(WKDIR,f_scan), 'wb') as f_out:
-            f_out.write(scan_headers)
-            f_out.write(f_in.read())
-    else: # else append to file on subsequent of type
-        with gzip.open(os.path.join(root,f), 'rb') as f_in, open(os.path.join(WKDIR,f_scan), 'ab') as f_out:
-            f_out.write(f_in.read())
-    scan_cnt[f_scan] = scan_cnt[f_scan] + 1
+    with gzip.open(os.path.join(root,f), 'rb') as f_in:
+        for line in f_in.readlines():
+            mode = line.decode().split(sep='|')[0]
+            if mode in ('1','2','3'):
+                mode_tp = mode + scan_tp
+            else: # mode not in 1 or 2 or 3
+                mode_tp = 'Oth' + scan_tp
+            with open(os.path.join(WKDIR,files_out[mode_tp]), 'ab') as f_out:
+                f_out.write(line)
+            scan_cnt[mode_tp] = scan_cnt[mode_tp] + 1
 
-
-# In[ ]:
-
+# In[main]:
 def main():
-    counter = 0
     for root,dirs,files in os.walk(WKDIR):
         for f in files:
             if f.endswith('.gz'):
-                if 'Samp_0' in root and  'On' in root:
-                    threads.append(threading.Thread(target=scan_create, args=(root,f,fo_s0on)))
+                if 'On' in root:
+                    threads.append(threading.Thread(target=scan_create, args=(root,f,'On')))
                     threads[-1].start()
-                elif 'Samp_0' in root and  'Off' in root:
-                    threads.append(threading.Thread(target=scan_create, args=(root,f,fo_s0off)))
-                    threads[-1].start()
-                elif 'Samp_1' in root and  'On' in root:
-                    threads.append(threading.Thread(target=scan_create, args=(root,f,fo_s1on)))
-                    threads[-1].start()
-                elif 'Samp_1' in root and  'Off' in root:
-                    threads.append(threading.Thread(target=scan_create, args=(root,f,fo_s1off)))
+                elif 'Off' in root:
+                    threads.append(threading.Thread(target=scan_create, args=(root,f,'Off')))
                     threads[-1].start()
         for thr in threads:
             thr.join()
     
 if __name__ == "__main__":
     global scan_cnt # initialise
-    scan_cnt['samp0_on']  = 0   
-    scan_cnt['samp0_off'] = 0
-    scan_cnt['samp1_on']  = 0
-    scan_cnt['samp1_off'] = 0
+    for k in scan_cnt.keys():
+        scan_cnt[k] = 0
 
     main() # need this for threading I believe
 
-
-# ### NOTE:
-# #### The below import command may be exceedingly slow as loading all into memory
-# 
-
-# In[ ]:
+# In[demo import]:
 
 import pandas as pd
-df = pd.read_csv(os.path.join(WKDIR,fo_s0off), sep='|')
-
+df = pd.read_csv(os.path.join(WKDIR,'On_Train'), sep='|')
